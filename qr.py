0c2c07ef9d03b4c162c1cb5b119374eebb356d32
kalDima <kalugin180806@gmail.com>
2021-07-06 16:27:37 +0300

    Updated

--- qr.py
+++ qr.py
@@ -16,8 +16,9 @@ def index():
         month = request.form.get('month')
         day = request.form.get('day')
         user_key = request.form.get('key')
-        with open(os.getcwd().replace("\\", "/") + "/key.txt", "r") as f:
+        with open(os.getcwd() + "/key.txt", "r") as f:
             original_key = f.readline()
+        print(user_key, original_key)
 
         if user_key == original_key and len(fio.split(" ")) == 3 and (int(day) > 0 and int(day) < 32):
             message = "Все данные введены верно, QR-код создан"
@@ -26,10 +27,10 @@ def index():
             patronymic = fio.split()[2][0] + ((len(fio.split()[2]) - 1) * "*")
             date = day + " " + month
             hash = hashlib.sha256((fio + " " + date).encode('utf-8')).hexdigest()[:12].upper()
-            file_qr = os.getcwd().replace("\\", "/") + "/templates/static/" + 'qr-' + hash + '.png'
-            file_html = os.getcwd().replace("\\", "/") + "/templates/static/" + 'qr-' + hash + '.html'
+            file_qr = os.getcwd() + "/templates/static/png/" + 'qr-' + hash + '.png'
+            file_html = os.getcwd() + "/templates/static/html/" + 'qr-' + hash + '.html'
             if not os.path.exists(file_qr):
-                with open(os.getcwd().replace("\\", "/") + "/templates/static/demo.html", "r") as f:
+                with open(os.getcwd() + "/templates/static/html/qr-demo.html", "r") as f:
                     demo_html = f.readline()
                 with open(file_html, "w") as f:
                     f.write(demo_html.replace("Имя", first_name).replace("Фамилия", second_name).replace("Отчество", patronymic).replace("дата", date))
@@ -46,7 +47,7 @@ def index():
 @app.route('/qr')
 def qr():
     id = request.args.get("id")
-    return render_template("qr-" + id + ".html")
+    return render_template("/static/html/qr-" + id + ".html")
 
 
 socketio = SocketIO()
