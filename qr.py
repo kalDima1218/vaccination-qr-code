d1ab6ff8f9ac20e516577eb32ea8a6209b9a47ba
kalDima1218 <kalugin180806@gmail.com>
2021-08-27 00:59:45 +0300

    New storing and displaying system

--- qr.py
+++ qr.py
@@ -1,11 +1,12 @@
 import os
+import base64
 import hashlib
 import pyqrcode
-#from flask_socketio import SocketIO
+from flask_socketio import SocketIO
 from flask import Flask, request, render_template, send_file
 
 app = Flask(__name__)
-app.debug = False
+app.debug = True
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -24,14 +25,11 @@ def index():
             second_name = fio.split()[1][0] + ((len(fio.split()[1]) - 1) * "*")
             patronymic = fio.split()[2][0] + ((len(fio.split()[2]) - 1) * "*")
             date = day + " " + month
-            hash = hashlib.sha256((fio + " " + date).encode('utf-8')).hexdigest()[:12].upper()
-            file_qr = os.getcwd() + "/templates/static/png/" + 'qr-' + hash + '.png'
-            file_html = os.getcwd() + "/templates/static/html/" + 'qr-' + hash + '.html'
+
+            hash = base64.b64encode(bytes(first_name + " " + second_name + " " + patronymic + " " + date, 'utf-8')).decode()
+
+            file_qr = os.getcwd() + "/templates/png/" + 'qr-' + hash + '.png'
             if not os.path.exists(file_qr):
-                with open(os.getcwd() + "/templates/static/html/qr-demo.html", "r") as f:
-                    demo_html = f.read()
-                with open(file_html, "w") as f:
-                    f.write(demo_html.replace("Имя", first_name).replace("Фамилия", second_name).replace("Отчество", patronymic).replace("дата", date))
                 url = pyqrcode.create("https://immune.mos.tmweb.ru:5000/qr?id=" + hash)
                 url.png(file_qr, scale=5)
             return send_file(file_qr, as_attachment=True, mimetype='image/png')
@@ -45,10 +43,11 @@ def index():
 @app.route('/qr')
 def qr():
     id = request.args.get("id")
-    return render_template("/static/html/qr-" + id + ".html")
+    data = base64.b64decode(bytes(id, 'utf-8')).decode().split(" ")
+    return render_template("qr-demo.html", message=data)
 
 
-#socketio = SocketIO()
-#socketio.init_app(app)
-#socketio.run(app, host="immune.mos.tmweb.ru")
-app.run(host="immune.mos.tmweb.ru", ssl_context=('cert.pem', 'privkey.pem'))
+socketio = SocketIO()
+socketio.init_app(app)
+socketio.run(app, host="immune.mos.tmweb.ru")
+#app.run(host="immune.mos.tmweb.ru", ssl_context=('cert.pem', 'privkey.pem'))
