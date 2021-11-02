e6b5aaab81153760937a6a76cf03f8b516b45e42
kalDima1218 <kalugin180806@gmail.com>
2021-11-02 15:35:45 +0300

    Edited qr-demo.html and added error message in index.html

--- qr.py
+++ qr.py
@@ -1,6 +1,5 @@
 import os
 import base64
-import hashlib
 import pyqrcode
 #from flask_socketio import SocketIO
 from flask import Flask, request, render_template, send_file
@@ -11,7 +10,6 @@ app.debug = False
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    #message = ''
     if request.method == 'POST':
         fio = request.form.get('fio')
         month = request.form.get('month')
@@ -20,7 +18,6 @@ def index():
         #with open(os.getcwd() + "/key.txt", "r") as f:
             #original_key = f.readline()
         if len(fio.split(" ")) == 3 and (int(day) > 0 and int(day) < 32) and month != "Выберите месяц рождения": #user_key == original_key
-            #message = "Все данные введены верно, QR-код создан"
             first_name = fio.split()[0][0] + ((len(fio.split()[0]) - 1) * "*")
             second_name = fio.split()[1][0] + ((len(fio.split()[1]) - 1) * "*")
             patronymic = fio.split()[2][0] + ((len(fio.split()[2]) - 1) * "*")
@@ -28,16 +25,16 @@ def index():
 
             hash = base64.b64encode(bytes(first_name + " " + second_name + " " + patronymic + " " + date, 'utf-8')).decode()
 
-            file_qr = os.getcwd() + "/templates/png/" + 'qr-' + hash + '.png'
-            if not os.path.exists(file_qr):
-                url = pyqrcode.create("https://immune.mos.tmweb.ru:5000/qr?id=" + hash)
-                url.png(file_qr, scale=5)
-            return send_file(file_qr, as_attachment=True, mimetype='image/png')
+            file_qr = os.getcwd() + "/qr-" + hash + ".png"
+            url = pyqrcode.create("https://immune.mos.tmweb.ru:5000/qr?id=" + hash)
+            url.png(file_qr, scale=5)
+            var = send_file(file_qr, as_attachment=True, mimetype='image/png')
+            os.remove(file_qr)
+            return var
         else:
-            #message = "Данные введены неверно"
-            return render_template('index.html')
+            return render_template('index.html', lh='30px', error_message='Данные введены неверно')
     else:
-        return render_template('index.html')
+        return render_template('index.html', lh='0px', error_message='')
 
 
 @app.route('/qr')
